ee210583217bc7205d2aae90bb4f0c67755875ac
RMIT <RMIT@Roshans-MacBook-Air.local>
2018-09-19 15:17:09 +1000

    added tables for main view

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -13,13 +13,10 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-
-		BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-		primaryStage.setTitle("Main Window");
-		Scene scene = new Scene(root,400,400);
-		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+        primaryStage.setTitle("My Contacts");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
 		
 	}
 	
